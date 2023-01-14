9cd901aafa4e0fabeb92e9a35a796e2b3696ac71
Kirill <cs1@list.ru>
2023-01-15 01:53:34 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,38 @@
-from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from places.models import Place, Image
+from django.templatetags.static import static
+
+def get_map_places() -> dict:
+    return {
+        "type":
+            "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.62, 55.793676]
+            },
+            "properties": {
+                "title": "Легенды Москвы",
+                "placeId": "moscow_legends",
+                "detailsUrl": static('places/moscow_legends.json')
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.64, 55.753676]
+            },
+            "properties": {
+                "title": "Крыши24.рф",
+                "placeId": "roofs24",
+                "detailsUrl": static('places/roofs24.json')
+            }
+        }]
+    }
+
 
 def show_map(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendere_page = template.render(context, request)
-    return HttpResponse(rendere_page)
+    data = {"map_places": get_map_places()}
+    return render(request, 'index.html', context=data)
