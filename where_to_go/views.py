ab5f205a4d40530de95e69bc6dea47e1e403c012
Kirill <cs1@list.ru>
2023-01-25 02:02:09 +0300

    Removed unnecessary wrapper in show_map() func

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -22,10 +22,10 @@ def get_map_place_details(request, place_id):
     return JsonResponse(place_details, json_dumps_params={'ensure_ascii': False, 'indent': 4})
 
 
-def get_map_places() -> dict:
+def show_map(request):
     places = Place.objects.all()
-
     map_places = {'type': 'FeatureCollection', 'features': []}
+
     for place in places:
         feature = {
             'type': 'Feature',
@@ -41,9 +41,5 @@ def get_map_places() -> dict:
         }
         map_places['features'].append(feature)
 
-    return map_places
-
-
-def show_map(request):
-    data = {'map_places': get_map_places()}
+    data = {'map_places': map_places}
     return render(request, 'index.html', context=data)
