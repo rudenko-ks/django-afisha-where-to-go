d3bdba9fe8af8fa3b96440ce1ee93762b8a7e420
Kirill <cs1@list.ru>
2023-01-24 22:44:28 +0300

    Used single-quoted strings style

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -10,13 +10,13 @@ def get_map_place_details(request, place_id):
     place = get_object_or_404(Place, id=place_id)
     place_images = Image.objects.filter(place=place_id)
     place_details = {
-        "title": place.title,
-        "imgs": [place_image.image.url for place_image in place_images],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lng": place.longitude,
-            "lat": place.latitude
+        'title': place.title,
+        'imgs': [place_image.image.url for place_image in place_images],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
         }
     }
     return JsonResponse(place_details, json_dumps_params={'ensure_ascii': False, 'indent': 4})
@@ -25,18 +25,18 @@ def get_map_place_details(request, place_id):
 def get_map_places() -> dict:
     places = Place.objects.all()
 
-    map_places = {"type": "FeatureCollection", "features": []}
+    map_places = {'type': 'FeatureCollection', 'features': []}
     for place in places:
         feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.longitude, place.latitude]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": reverse('place-details', args=[place.id])
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': reverse('place-details', args=[place.id])
             }
         }
         map_places['features'].append(feature)
